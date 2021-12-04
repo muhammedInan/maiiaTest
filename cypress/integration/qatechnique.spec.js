6f9bf526a26dce682cfa4455e2a7a90872b122fe
muhammedInan <muhammed-inan@outlook.com>
2021-12-04 22:45:12 +0100

    cliqué sur la suggestion proposée

--- cypress/integration/qatechnique.spec.js
+++ cypress/integration/qatechnique.spec.js
@@ -2,5 +2,6 @@ describe("test technique", ()=>{
     it("choix du médecin", ()=>{
         cy.visit("https://www.staging.maiia.com/")
         cy.get('#downshift_input_0').type("test qatechnique")
+        cy.get(".search-item").click()
     })
 })
