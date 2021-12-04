a97d5f6868b7af91b91b476351b3d6fb6d401bc4
muhammedInan <muhammed-inan@outlook.com>
2021-12-04 23:11:20 +0100

    finalisation

--- cypress/pageObjectModel/search.js
+++ cypress/pageObjectModel/search.js
@@ -3,6 +3,22 @@ class search{
     searchDoctor(){
         return cy.get('#downshift_input_0')
     }
+
+    specialiste(){
+        return cy.get(".search-item")
+    }
+
+    choiceSelect(){
+        return cy.get("#consultationReasonName")
+    }
+
+    consultationReason(){
+        return cy.get('[data-cy="Ablation de fils"]')
+    }
+
+    calendar(){
+        return cy.get('[class="search-card__rdv availability-calendar percy-hidden"]')
+    }
 }
 
 export default search
