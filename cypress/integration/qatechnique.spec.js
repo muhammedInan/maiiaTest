75f5a8f7b9819864b8c42e732ef18ec6f6587346
muhammedInan <muhammed-inan@outlook.com>
2021-12-04 22:43:29 +0100

    chercher dans la barre de recherche

--- cypress/integration/qatechnique.spec.js
+++ cypress/integration/qatechnique.spec.js
@@ -1,5 +1,6 @@
 describe("test technique", ()=>{
     it("choix du médecin", ()=>{
         cy.visit("https://www.staging.maiia.com/")
+        cy.get('#downshift_input_0').type("test qatechnique")
     })
 })
